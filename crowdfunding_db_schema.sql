33ee2f290837d9dbf6b0c2fd5a0b7b017f2b2761
Asres Dagnew <asres2009.2@gmail.com>
2025-03-13 20:01:25 -0700

    Final

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE subcategory (
 
 -- Create the Contacts Table
 CREATE TABLE contacts (
-    contact_id SERIAL PRIMARY KEY,  -- Unique contact ID
+    contact_id INT PRIMARY KEY,  -- Unique contact ID
     first_name VARCHAR(100),        -- First name of the contact person
     last_name VARCHAR(100),         -- Last name of the contact person
     email VARCHAR(255) UNIQUE       -- Unique email address of the contact person
@@ -29,12 +29,12 @@ CREATE TABLE contacts (
 
 -- Create the Campaign Table
 CREATE TABLE campaign (
-    cf_id SERIAL PRIMARY KEY,       -- Unique campaign ID 
+    cf_id INT PRIMARY KEY,       -- Unique campaign ID 
     contact_id INT,                 -- Foreign key linking to the Contacts table
     company_name VARCHAR(255),      -- Name of the company running the campaign
-    description TEXT,               -- Description of the campaign
-    goal DECIMAL(10,2),             -- Fundraising goal (stored as decimal)
-    pledged DECIMAL(10,2),          -- Amount pledged by backers (stored as decimal)
+    description VARCHAR,               -- Description of the campaign
+    goal FLOAT,             -- Fundraising goal 
+    pledged FLOAT,          -- Amount pledged by backers 
     outcome VARCHAR(50),            -- Outcome of the campaign (e.g., "successful", "failed")
     backers_count INT,              -- Number of backers supporting the campaign
     country VARCHAR(50),            -- Country where the campaign was launched
@@ -44,8 +44,4 @@ CREATE TABLE campaign (
     category_id VARCHAR(10),        -- Foreign key linking to the Category table
     subcategory_id VARCHAR(10),     -- Foreign key linking to the Subcategory table,
     
-    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE,
-    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id) ON DELETE CASCADE
 );
-
